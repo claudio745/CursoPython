3af3a861b49d23d96db9f68a0870fb9d672d2541
claudio afaro <74275767+claudio745@users.noreply.github.com>
2023-07-05 15:20:17 -0400

    Clase 05/07/2023 Periodo 1

---
+++ Operadores-Aritmeticos.py
@@ -0,0 +1,27 @@
+print("Suma")
+print(2 + 3)
+
+print("Resta")
+print(5 - 2)
+
+print("Multiplicacion")
+print(5 * 2)
+
+print("Division")
+print(15 / 3)
+
+print("Modulo")
+print(7 % 3)
+
+print("Exponente")
+print(2 ** 3)
+
+print("Orden de predencia para operaciones aritmeticas")
+print("Parentesis, potencias, raices, multiplicación, división, suma y la resta")
+print(5 + 5 * 2)
+
+print("Tarea")
+print("Como hacer una raiz cuadrada")
+print(25**(1/2))
+print("Averiguar que es la división entera")
+
---
+++ Tipos de datos.py
@@ -0,0 +1,48 @@
+# TIPOS DE DATOS 
+# EN PYTHON Todo es un objeto
+
+# Tipos de datos
+# 1.- Numeros enteros
+# 2.- Numeros decimales
+# 3.- Cadenas de texto
+# 4.- Booleanos
+
+
+# 1.- Numeros enteros (int, integer)
+# 1 2 3 500 9000
+
+# 2.- Numeros decimales (float, flotantes)
+# 3.5  3.1416237876   4.0
+
+decimal = 25.36
+
+# 3.- Cadenas de texto (str, string)
+cadena_de_texto = "Esto es una 'cadena' de texto"
+comillas_simples = 'esto es otra "cadena" de texto'
+comillas_triples = """comillas dobles 'comillas simples'
+aca iria mas
+informacion
+"""
+# 3.1.- Concatenacion
+cadena_1 = "Hola "
+cadena_2 = "Mundo"
+print(cadena_1 + cadena_2)
+
+# 4.- Booleanos (True, False)
+
+es_estudiante = True
+trabaja = False
+
+# Convertir tipos de datos
+
+numero_1 = input("Ingrese un numero 1: ")
+numero_2= input("Ingrese un numero 2: ")
+
+print(int(numero_1) + int(numero_2))
+
+
+decimal_2 = 125.12
+print(int(decimal_2))
+
+numero_entero = 45
+print(float(numero_entero))
